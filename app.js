c2e68a966017bffd97682c88ca6d3fc71f497128
Matthieu Auger <mail@matthieuauger.com>
2014-01-30 21:48:32 +0100

    Use Google client for requests

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
+/*
+    Require and global initialization
+*/
 var express = require('express');
-var routes = require('./routes');
 var config = require('./config');
-var http = require('http');
 var path = require('path');
 var request = require('request');
-var doorClient = require('./lib/client/doorClient');
-var googleapis = require('googleapis'),
-    OAuth2 = googleapis.auth.OAuth2;
 
-var oauth2Client = new OAuth2(config.APP_ID, config.APP_SECRET, config.REDIRECT_URI);
+var doorClient = require('./lib/client/doorClient');
+var googleapis = require('googleapis');
+var OAuth2 = googleapis.auth.OAuth2;
 
 var app = express();
 
@@ -25,39 +25,41 @@ app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+if ('development' == app.get('env')) {  
+    app.use(express.errorHandler());
 }
 
+// Création of Google client
+var oauth2Client = new OAuth2(config.APP_ID, config.APP_SECRET, config.REDIRECT_URI);
+    
 app.get('/', function(req, res){
-
 	var authUrl = oauth2Client.generateAuthUrl({
-	  access_type: 'offline',
-	  scope: 'https://www.googleapis.com/auth/userinfo.email',
-	  state: 'profile',
-	  approval_prompt: 'force'
+        access_type: 'offline',
+        scope: 'https://www.googleapis.com/auth/userinfo.email',
+        state: 'profile',
+        approval_prompt: 'force'
 	});
   
   res.render('index', { authUrl: authUrl });
 });
 
 /**
-* Callback function called after oauth authentivation
+* Callback function called after oauth authentication
 * We get the code from the user, and get a refresh_token from Google with this code
 * The refresh_token is then sent back to the user in order to store it in local storage
 **/
 app.get('/oauthcallback', function(req, res) {
-        var state = req.query.state;
-        //TODO : Check that states match
-        var code = req.query.code;
+    //TODO : Check that states match
+    var state = req.query.state;
+    var code = req.query.code;
 
-        oauth2Client.getToken(code, function(err, tokens) {
-		  	if (!"refresh_token" in tokens) {
-                return res.send("Le processus d'authentification a échoué");        
-            }
-                
-            res.redirect("/?refresh_token=" + tokens.refresh_token);
-		});
+    oauth2Client.getToken(code, function(err, tokens) {
+	  	if (!"refresh_token" in tokens) {
+            return res.send("Le processus d'authentification a échoué");        
+        }
+            
+        res.redirect("/?refresh_token=" + tokens.refresh_token);
+	});
 });
 
 /**
@@ -68,71 +70,31 @@ app.get('/oauthcallback', function(req, res) {
 * If allomatch, we get username and password from request and hit the API of allomatch in order to grant (or not) the access 
 **/
 app.post('/api/opendoor', function(req, res) {
-        var auth_type = req.body.auth_type;
+        var refresh_token = req.body.refresh_token;
 
-        if ("googleoauth" == auth_type) {
-                var refresh_token = req.body.refresh_token;
+        oauth2Client.credentials = {
+		  refresh_token: refresh_token
+		};
 
-                getAccessTokenFromRefreshToken(refresh_token, function (e, r, body) {
-                        var body = JSON.parse(body);
-
-                        if (!"access_token" in body) {
-                                return res.send({status: -1, message: "L'authentification Google Plus a échoué (code 1)"});
+		googleapis
+			.discover('oauth2', 'v1')
+			.execute(function(err, client) {
+				if(!err) {
+		      		client.oauth2.userinfo.get().withAuthClient(oauth2Client).execute(function(err, results){
+			      		if (!"hd" in body) {
+                                return res.send({status: -1, message: "L'authentification Google Plus a échoué (code 2)"});
+                        } else if ("theodo.fr" !== body.hd) {
+                        	console.log(body);
+                                return res.send({status: -1, message: "L'authentification Google Plus a échoué (l'adresse fournie n'appartient pas à Theodo)"});
                         }
 
-                        getInfoFromAccessToken(body.access_token, function(e, r, body) {
-                                var body = JSON.parse(body);
-
-                                if (!"hd" in body) {
-                                        return res.send({status: -1, message: "L'authentification Google Plus a échoué (code 2)"});
-                                } else if ("theodo.fr" !== body.hd) {
-                                	console.log(body);
-                                        return res.send({status: -1, message: "L'authentification Google Plus a échoué (l'adresse fournie n'appartient pas à Theodo)"});
-                                }
-
-                                doorClient.open();
-                                res.send({status: 0, message: 'Door opened. Welcome !'});
-                        });
-                });
-        }
-        else if ("allomatch" == auth_type) {
-                var username = req.body.username;
-                var password = req.body.password;
-
-                data = {
-                        username: username,
-                        password: password
-                };
-
-                request.post(config.ALLOMATCH_DOOR_URL, {form: data}, function (e, r, body) {
-                        if ("true" == body) {
-                                theodoDoorClient.open();
-                            res.send({status: 0, message: 'Door opened. Welcome !'});
-                        }
-                        else {
-                                res.send({status: -1, message: "L'authentification Allomatch a échoué"});
-                        }
-                });
-        }
+                        doorClient.open();
+                        res.send({status: 0, message: 'Door opened. Welcome !'});
+			  		});
+		  		}
+		    });
 });
 
-function getAccessTokenFromRefreshToken(refresh_token, callback)
-{
-        data = {
-                refresh_token: refresh_token,
-                client_id: config.APP_ID,
-                client_secret: config.APP_SECRET,
-                grant_type: "refresh_token"
-        };
-
-        request.post(config.GOOGLE_API_TOKEN_URL, {form: data}, callback);
-}
-
-function getInfoFromAccessToken(access_token, callback)
-{
-        request.get(config.GOOGLE_API_USERINFO_URL + access_token, callback);
-}
-
-http.createServer(app).listen(app.get('port'), function(){
+require('http').createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
