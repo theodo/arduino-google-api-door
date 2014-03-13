53559703ce7fb9500f65a574017b8de1a37d5881
Matthieu Auger <mail@matthieuauger.com>
2014-03-13 21:12:22 +0100

    Add some comments and remove the type variable. We only use Google in this demo

--- public/javascripts/door.js
+++ public/javascripts/door.js
@@ -1,6 +1,7 @@
 // Theses variables will be used when Door button is pressed
 var current_refresh_token = null;
 
+/* Allow to get a parameter in the URL ex: ?foo=bar */
 function getURLParameter(name) {
     return decodeURI((RegExp(name + '=' + '(.+?)(&|$)').exec(location.search) || [, null])[1]);
 }
@@ -8,10 +9,12 @@ function getURLParameter(name) {
 $(document).ready(function() {
     var refresh_token = localStorage.getItem('refresh_token');
     
+    /* First we try to fetch the refresh_token in the local storage */
     if (null !== refresh_token) {
         current_refresh_token = refresh_token;
         $("#openDoor").removeClass("disabled").addClass("btn-success");
     } else {
+        /* Otherwise, we are maybe in the authentication process and the server gaves us the refresh token */
         refresh_token = getURLParameter("refresh_token");
         if ("null" !== refresh_token) {
             current_refresh_token = refresh_token;
@@ -25,7 +28,6 @@ $("#openDoor").on("click", function() {
     var data = {};
     
     if (null !== refresh_token) {
-        data.auth_type = "googleoauth";
         data.refresh_token = refresh_token;
     } else {
         return;
@@ -36,6 +38,7 @@ $("#openDoor").on("click", function() {
         url: '/api/opendoor',
         success: function(result) {
             $("#status").html(result.message);
+            /* If the app tell us that the authentication is good, we set the token inside the local storage of the user */
             if (result.status == 0) {
                 localStorage.setItem('refresh_token', refresh_token);
             }
@@ -47,7 +50,6 @@ $("#openDoor").on("click", function() {
 $("#reinitialize").on("click", function() {
     // Remove actual used variables
     current_refresh_token = null;
-    // Clear local storage
     localStorage.removeItem('refresh_token');
     $("#openDoor").addClass("disabled").removeClass("btn-success");
 });
